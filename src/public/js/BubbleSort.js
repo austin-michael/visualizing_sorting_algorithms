39ee4f4aa78677a1053cbebc1b177021b539fd61
austin-michael <austin.michaelwill@gmail.com>
2019-12-14 16:37:38 -0700

    implemented the basic bar chart for the bubble sort

--- src/public/js/BubbleSort.js
+++ src/public/js/BubbleSort.js
@@ -2,29 +2,48 @@ class BubbleSort {
     constructor (unsortedArray) {
         this.unsortedArray = unsortedArray;
 
-        this.sort(unsortedArray);
+        this.drawBars(this.unsortedArray)
+        this.sortedArray = this.sort(unsortedArray);
+        //this.drawBars(this.sortedArray);
     }
 
-    sort (unsortedArray) {
-        console.log(unsortedArray);
+    sort (data) {
+        //console.log(unsortedArray);
 
         var isSorted = false;
         while (!isSorted) {
             isSorted = true;
-            for (var i = 0; i < unsortedArray.length - 1; i++){
-                if (unsortedArray[i] > unsortedArray[i + 1]) {
-                    this.swap(unsortedArray, i, i + 1);
+            for (var i = 0; i < data.length - 1; i++){
+                if (data[i] > data[i + 1]) {
+                    this.swap(data, i, i + 1);
                     isSorted = false
                 }
             }
         }
 
-        console.log(unsortedArray)
+        //console.log(unsortedArray)
+        return data;
     }
 
     swap (unsortedArray, i, j) {
         var temp = unsortedArray[i];
         unsortedArray[i] = unsortedArray[j];
         unsortedArray[j] = temp;
+    }
+
+    drawBars (data) {
+        console.log(data);
+
+        var scale = d3.scale.linear()
+            .domain([0, d3.max(data)])
+            .range([0, 420]);
+        
+        d3.select(".chart")
+            .selectAll("div")
+                .data(data)
+            .enter().append("div")
+                .style("width", function(d) { return scale(d) + "px"; })
+                .text(function(d) { return d; })
+        ;
     }
 }
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -4,6 +4,6 @@ function genRandomArray(length, max) {
     });
 }
 
-let unsortedArray = genRandomArray(5, 100);
+let unsortedArray = genRandomArray(20, 20);
 
 let bubblesort = new BubbleSort(unsortedArray);
